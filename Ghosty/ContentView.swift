730967c5e431519018a99aca0706c2a69ee49f26
Ben Shabowski <benshabowski@gmail.com>
2022-09-06 17:25:38 -0500

    Initial Commit

---
+++ Ghosty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ghosty
+//
+//  Created by Benjamin Shabowski on 9/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is ghosty")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ghosty/GhostyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostyApp.swift
+//  Ghosty
+//
+//  Created by Benjamin Shabowski on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GhostyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
